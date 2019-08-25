75804a2c34e5d7fce7411135b8c19a37a1452706
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-25 20:36:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  phrase = "The line is currently: ."
+  if katz_deli > 0 
+    return 
+  else
+    "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts #person at end of line's name along with position in line
+end
+
+def now_serving(name)
+  if #ppl in line
+    puts #next person in line and then remove them from front
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
